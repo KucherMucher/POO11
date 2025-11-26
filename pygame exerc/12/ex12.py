f970d55c00f9c6d2023a5f346bfc324d1d084b04
Kucher Mucher <a10653@csmiguel.pt>
2025-11-26 14:47:02 +0000

    ended ex12

--- pygame exerc/12/ex12.py
+++ pygame exerc/12/ex12.py
@@ -5,7 +5,7 @@ pygame.init()
 
 
 
-fonte = pygame.font.Font(None, 40)
+fonte = pygame.font.Font(None, 30)
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -28,15 +28,14 @@ while running:
         for culumn in range(8):
             color = white if cor else black
             pygame.draw.rect(screen, color, (rect_coord[0]+(rect_size*culumn), rect_coord[1]+(rect_size*raw), rect_size, rect_size))
-            screen.blit(fonte.render(f"{chr(65+culumn)}", True, (0, 0, 0)), (rect_coord[0]+13+ rect_size*culumn, 50))
-
             
-        
+            screen.blit(fonte.render(f"{chr(65+culumn)}", True, (0, 0, 0)), (rect_coord[0]+ 17+ rect_size*culumn, rect_coord[1]-rect_size/2))
+            screen.blit(fonte.render(f"{chr(65+culumn)}", True, (0, 0, 0)), (rect_coord[0]+17+ rect_size*culumn, rect_coord[1]+rect_size*8+10))
+            screen.blit(fonte.render(f"{1+raw}", True, (0, 0, 0)), (rect_coord[0]-rect_size/2, rect_coord[0]+14+rect_size*raw)) #é assim as coordenadas dos textos para fazer
+            screen.blit(fonte.render(f"{1+raw}", True, (0, 0, 0)), (rect_coord[0]+rect_size*8+10, rect_coord[0]+14+rect_size*raw))
+
             cor = not cor
 
-        
-        
-    
     pygame.display.update()
 
 pygame.quit()
