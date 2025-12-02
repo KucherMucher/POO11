36c4b158a0fb4886087e6c181cb846d3b338ec46
Kucher Mucher <a10653@csmiguel.pt>
2025-12-02 11:06:05 +0000

    completed ex13

--- pygame exerc/13/ex13.py
+++ pygame exerc/13/ex13.py
@@ -1,12 +1,13 @@
 import pygame
 import sys
 import random
+import math
 
 
 pygame.init()
 
 
-width, height = 800, 600
+width, height = 1080, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Simulação do carro")
 
@@ -21,11 +22,9 @@ beige = (200, 200, 180)
 #físicas do carro
 pos_x = 400
 pos_y = 400
+distance = 0
 
-v_0 = 0
-v_x = 0
-acel = 1
-t = 0
+vel = 0
 
 
 running = True
@@ -35,10 +34,41 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        vel-=1
+    elif keys[pygame.K_RIGHT]:
+        vel+=1
+    elif keys[pygame.K_SPACE]:
+        if vel > 0:
+            vel -= 1
+        elif vel < 0:
+            vel += 1
+            
+
+    pos_x = pos_x+vel
+
+    distance = distance + math.sqrt((vel**2))
+
+    #if (keys[pygame.K_a] == False or keys[pygame.K_d] == False):
+        #if v_0 == 0:
+
+    if pos_x + size_carro[0] < 0:
+        pos_x = width
+    elif pos_x - size_carro[0] > width:
+        pos_x = 0 - size_carro[0]
+
+
+    text = font.render(f"Velocidade atual: {vel} m/s", True, (250, 250, 250))
+    text2 = font.render(f"Distância percorrida: {distance} pix.", True, (250, 250, 250))
     screen.fill((0, 120, 0))
-    pygame.draw.rect(screen, (100, 100, 100), (0, 410, 800, 100))
+    pygame.draw.rect(screen, (100, 100, 100), (0, 410, width, 100))
     carro = pygame.draw.rect(screen, beige, ((pos_x, pos_y), size_carro))
+
+    screen.blit(text, (10, 10))
+    screen.blit(text2, (10, 50))
+
     pygame.display.flip()
     clock.tick(60)
 
