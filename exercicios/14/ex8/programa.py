bed37f38d8db458546dedf24498363b111aa2070
Kucher Mucher <a10653@csmiguel.pt>
2026-01-20 16:28:31 +0000

    ended ex8

--- exercicios/14/ex8/programa.py
+++ exercicios/14/ex8/programa.py
@@ -1,1 +1,16 @@
-import conversoes as c
+import conversoes as cf
+
+escolha = input("Insire temperatura (em Celcius ou Farenheit): ")
+
+valorf = 0
+valor = float((''.join(filter(lambda x: x.isdigit() or x == '.', escolha))).strip())
+
+
+print(escolha, valor)
+
+if escolha.endswith('F'):
+    valorf = cf.fahrenheit_para_celsius(valor)
+elif escolha.endswith('C'):
+    valorf = cf.celsius_para_fahrenheit(valor)
+
+print(round(valorf, 2))
