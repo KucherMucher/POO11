82a3f50d53b86d92baf1e85eff3e635011d3c916
Kucher Mucher <a10653@csmiguel.pt>
2026-01-20 11:05:30 +0000

    made generation code, works. Needs upgrades and bugfixes for other generations modes

--- pygame_1/2d_cave_generation.py
+++ pygame_1/2d_cave_generation.py
@@ -15,50 +15,99 @@ fps = pygame.time.Clock()
 tile_coord = [0, 0]
 tile_size = width/100
 tile_list = []
-empty = 0
+
+ind = 1
 
 class Tile:
-    def __init__(self, row, column, tile):
+    def __init__(self, row, column, tile, empty=False):
         self.r = row
         self.c = column
         self.t = tile
+        self.e = empty
 
-for r in range(100):
-    for c in range(100):
-        if random.randint(2, 3) == 3:
-            tile_list.append(Tile(r, c, "empty"))
-            """
-            Ideias:
-            create lists in a list for columns and rows to easily acess each parameter
-            or
-            somehow use a class to ffind out the tiles neighbours
-            """
-            empty += 1
+def white_noise():
+    for r in range(100):
+        for c in range(100):
+            if random.randint(3, 5) == 3:
+                tile_list.append(Tile(r, c, None, True))
+                """
+                Ideias:
+                create lists in a list for columns and rows to easily acess each parameter
+                or
+                somehow use a class to ffind out the tiles neighbours:
+                    cound every neighbour using class
+                    find and anihilate tiles using class
+                """
+                continue
+            else:
+                tile = (tile_coord[0]+(tile_size*c), tile_coord[1]+(tile_size*r), tile_size, tile_size)
+                tile_list.append(Tile(r, c, tile))
+                pygame.draw.rect(screen, grey, tile)
+
+
+def hello_neighbour(current_tile):
+    n = 0
+    for nr in range(-1, 2):
+        for nc in range(-1, 2):
+            if nr == 0 and nc == 0:
+                continue
+            my_dear_neighbour = next((tile for tile in tile_list if tile.r == current_tile.r + nr and tile.c == current_tile.c + nc and not tile.e), None)
+
+            if my_dear_neighbour:
+                n+=1
+    #print(f"\n{current_tile.r, current_tile.c, n}")
+    return n
+
+def ANIHILATE():
+    for tile in tile_list:
+        if tile.r == 0 or tile.r == 99:
+            continue
+        elif tile.c == 0 or tile.c == 99:
             continue
         else:
-            rect = (tile_coord[0]+(tile_size*c), tile_coord[1]+(tile_size*r), tile_size, tile_size)
-            tile_list.append(Tile(r, c, rect))
-            pygame.draw.rect(screen, grey, rect)
+            my_neighbour_n = hello_neighbour(tile)
 
-def hello_neighbour():
-    for i in range(10000):
-        tile = tile_list[i]
-        n = 8
-        #if tile["r"] == 
+            if my_neighbour_n < 4:
+                tile.t = None
+                tile.e = True
 
-def kys():
-    for i in range(10000):
-        tile = tile_list[i]
-        if tile["tile"] != "empty":
-            my_neighbour_n = hello_neighbour()
+def ANIHILATE_loop():
+    for i in range(1, 5):
+        ANIHILATE()
+
+def CLEANUP():
+    for tile in tile_list:
+        if (tile.r == 0 or tile.r == 99) and tile.e:
+            tile.t = (tile_coord[0]+(tile_size*tile.c), tile_coord[1]+(tile_size*tile.r), tile_size, tile_size)
+            tile.e = False
+        elif (tile.c == 0 or tile.c == 99) and tile.e:
+            tile.t = (tile_coord[0]+(tile_size*tile.c), tile_coord[1]+(tile_size*tile.r), tile_size, tile_size)
+            tile.e = False
+        else:
+            my_neighbour_n = hello_neighbour(tile)
+
+            if my_neighbour_n >= 5 and tile.e == True:
+                tile.t = (tile_coord[0]+(tile_size*tile.c), tile_coord[1]+(tile_size*tile.r), tile_size, tile_size)
+                tile.e = False
+def CLEANUP_2():
+    for tile in tile_list:
+        my_neighbour_n = hello_neighbour(tile)
+
+        if my_neighbour_n == 8 and tile.e == True:
+            tile.t = (tile_coord[0]+(tile_size*tile.c), tile_coord[1]+(tile_size*tile.r), tile_size, tile_size)
+            tile.e = False
+def generate_cave():
+    white_noise()
+    ANIHILATE_loop()
+    CLEANUP()
+    CLEANUP_2()
 
 def draw_cave():
-    kys()
-    for i in range(10000):
-        tile = tile_list[i]
-        if tile["tile"] != "empty":
-            pygame.draw.rect(screen, grey, tile["tile"])
+    for tile in tile_list:
+        if not tile.e:
+            pygame.draw.rect(screen, grey, tile.t)
 
+generate_cave()
 running = True
 while running:
     for event in pygame.event.get():
@@ -68,7 +117,6 @@ while running:
     screen.fill((0, 0, 0))
     
     draw_cave()
-
 
     pygame.display.update()
 
