175088d50b65f517be6927d0f83c49456172f847
Kucher Mucher <a10653@csmiguel.pt>
2026-01-21 14:24:30 +0000

    ended ex8 (14)

--- exercicios/14/ex8/programa.py
+++ exercicios/14/ex8/programa.py
@@ -2,15 +2,15 @@ import conversoes as cf
 
 escolha = input("Insire temperatura (em Celcius ou Farenheit): ")
 
-valorf = 0
 valor = float((''.join(filter(lambda x: x.isdigit() or x == '.', escolha))).strip())
 
 
-print(escolha, valor)
+try:
+    if escolha.upper().endswith('F'):
+        valorf = cf.fahrenheit_para_celsius(valor)
+    elif escolha.upper().endswith('C'):
+        valorf = cf.celsius_para_fahrenheit(valor)
 
-if escolha.endswith('F'):
-    valorf = cf.fahrenheit_para_celsius(valor)
-elif escolha.endswith('C'):
-    valorf = cf.celsius_para_fahrenheit(valor)
-
-print(round(valorf, 2))
+    print(round(valorf, 2))
+except Exception:
+    print("Não foi introduzida a escala")
