19867355b89152e83898a465901133a1ae14268e
Kucher Mucher <a10653@csmiguel.pt>
2026-02-04 15:21:22 +0000

    v0.0 (starting)

--- my_lil_projects/fetch-info-from-website/THEREALFWEBCRAPPER.py
+++ my_lil_projects/fetch-info-from-website/THEREALFWEBCRAPPER.py
@@ -13,12 +13,44 @@ import pdfkit
 import io
 
 driver = wd.Firefox()
+"""
 config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
 options={
     "print-media-type": None,
     "enable-local-file-access": None,
 }
+"""
+# get the link
+print("Triathlon get results python script.")
+root_ = "https://www.federacao-triatlo.pt/ftp2015/competicoes/resultados/resultados-2025/" #input("Link: ")
 
+# analise root with bs4
+root_response = requests.get(root_)
+root_soup = bs4(root_response.content, "html.parser")
 
+tabs = root_soup.find_all("div", class_="results-list__item")
+
+# for each tab we will see which registation case it is and then do the thing for specific case
+for tab in tabs:
+    tab_name = tab.text
+    tab_href = tab.find('a').get('href')
+    
+    # access soup to determine the case
+    tab_response = requests.get(tab_href)
+    tab_soup = bs4(tab_response.content, "html.parser")
+
+    # now we need to find key elements to see which case of the registation do we have
+    # . ftp-folder + bootstrap table (needs navigation in divisions "")
+    # . ftp-folder + pdf
+
+    # . bootstrap table (needs navigation in divisions "")
+    
+    # . external site + bootstrap table (needs navigation in divisions)
+    CASE = 0
+    ftp_folder = tab_soup.find_all('div', class_='ftp-folders')
+    table = 0
+    if :
+        CASE = 1.0
+        
 
 driver.quit()
